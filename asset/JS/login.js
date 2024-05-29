03fbca101baa5c52d447cd11ca0e3fb749747910
SoChiSoSad <binzinzim@gmail.com>
2024-05-30 04:18:31 +0700

    add

--- asset/JS/login.js
+++ asset/JS/login.js
@@ -24,6 +24,151 @@ togglePasswordVisibility();
 
 
 // đăng nhập bằng tài khoản đã được cấp
+// const loginButton = document.querySelector('.btn');
+
+// loginButton.addEventListener('click', async () => {
+//     const username = document.getElementById('Tai-Khoan').value;
+//     const password = document.getElementById('mat-khau').value;
+    
+//     // Kiểm tra xem người dùng đã nhập đầy đủ thông tin chưa
+//     if (username === '' || password === '') {
+//         alert('Vui lòng nhập đầy đủ tài khoản và mật khẩu');
+//         return;
+//     }
+    
+//     try {
+//         const response = await fetch('http://localhost:8000/api/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ username, password }),
+//         });
+
+//         if (response.ok) {
+//             const data = await response.json();
+//             // Giả sử server trả về một đối tượng với thuộc tính 'success' để chỉ ra đăng nhập thành công
+//             if (data.success && data.token && username == username || password == password) {
+//                 alert('Đăng nhập thành công!');
+
+//                 // Lưu token vào localStorage
+//                 localStorage.setItem('authToken', data.token);
+//                 // Giải mã token để lấy thông tin role
+//                 const decodedToken = jwtDecode(data.token);
+//                 const userRole = decodedToken.role;
+
+//                 // Redirect hoặc thực hiện hành động sau khi đăng nhập thành công
+//                 window.location.href = '../html/trang_home.html';
+//             } else {
+//                 alert('Tên đăng nhập hoặc mật khẩu không đúng, xin vui lòng nhập lại!');
+//             }
+//         } else {
+//             // Xử lý lỗi HTTP
+//             alert('Đã xảy ra lỗi khi đăng nhập, xin vui lòng thử lại sau!');
+//         }
+//     } catch (error) {
+//         // Xử lý lỗi mạng hoặc lỗi khác
+//         console.error('Error:', error);
+//         alert('Đã xảy ra lỗi khi kết nối tới server, xin vui lòng thử lại sau!');
+//     }
+// });
+
+// function jwtDecode(token) {
+//     try {
+//         const base64Url = token.split('.')[1];
+//         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+//             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//         }).join(''));
+
+//         return JSON.parse(jsonPayload);
+//     } catch (error) {
+//         console.error('Invalid token', error);
+//         return null;
+//     }
+// }
+// kết thúc đăng nhập
+
+
+// const loginButton = document.querySelector('.btn');
+
+// loginButton.addEventListener('click', async () => {
+//     const username = document.getElementById('Tai-Khoan').value;
+//     const password = document.getElementById('mat-khau').value;
+    
+//     // Kiểm tra xem người dùng đã nhập đầy đủ thông tin chưa
+//     if (username === '' || password === '') {
+//         alert('Vui lòng nhập đầy đủ tài khoản và mật khẩu');
+//         return;
+//     }
+    
+//     try {
+//         const response = await fetch('http://localhost:8000/api/login', {
+//             method: 'POST',
+//             headers: {
+//                 'Content-Type': 'application/json',
+//             },
+//             body: JSON.stringify({ username, password}),
+//         });
+
+//         if (response.ok) {
+//             const data = await response.json();
+//             // Giả sử server trả về một đối tượng với thuộc tính 'success' để chỉ ra đăng nhập thành công
+//             if (data.success && data.user.role && data.user.token && username == username || password == password) {
+//                 alert('Đăng nhập thành công!');
+//                 window.location.href = '../html/trang_home.html';
+//                 // Lưu token vào localStorage
+//                 localStorage.setItem('Token', data.user.token);
+//                 localStorage.setItem('Role', data.user.role);
+//                 // Giải mã token để lấy thông tin role
+//                 const decodedToken = jwtDecode(data.user.token);
+//                 const userRole = decodedToken.user.role;
+
+//                 // Điều hướng dựa trên role của người dùng
+//                 if (userRole === 'admin') {
+//                     window.location.href = '../html/phonglanhdao.html';
+//                     window.location.href = '../html/lanhdaodonvi.html';  
+//                     window.location.href = '../html/lanhdaotruong.html';
+//                     window.location.href = '../html/cacnhansu.html';
+//                 } else if (userRole === 'leader') {
+//                     window.location.href = '../html/lanhdaodonvi.html';  
+//                     window.location.href = '../html/lanhdaotruong.html';
+//                     window.location.href = '../html/cacnhansu.html'; 
+//                 } else if (userRole === 'personnel') {
+//                     window.location.href = '../html/cacnhansu.html';
+//                 } else {
+//                     alert('Vai trò của bạn không được hỗ trợ!');
+//                 }
+//             } else {
+//                 alert('Tên đăng nhập hoặc mật khẩu không đúng, xin vui lòng nhập lại!');
+//             }
+//         } else {
+//             // Xử lý lỗi HTTP
+//             alert('Đã xảy ra lỗi khi đăng nhập, xin vui lòng thử lại sau!');
+//         }
+//     } catch (error) {
+//         // Xử lý lỗi mạng hoặc lỗi khác
+//         console.error('Error:', error);
+//         alert('Đã xảy ra lỗi khi kết nối tới server, xin vui lòng thử lại sau!');
+//     }
+// });
+
+// function jwtDecode(token) {
+//     try {
+//         const base64Url = token.split('.')[1];
+//         const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+//         const jsonPayload = decodeURIComponent(atob(base64).split('').map(function(c) {
+//             return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+//         }).join(''));
+
+//         return JSON.parse(jsonPayload);
+//     } catch (error) {
+//         console.error('Invalid token', error);
+//         return null;
+//     }
+// }
+
+
 const loginButton = document.querySelector('.btn');
 
 loginButton.addEventListener('click', async () => {
@@ -47,12 +192,13 @@ loginButton.addEventListener('click', async () => {
 
         if (response.ok) {
             const data = await response.json();
-            // Giả sử server trả về một đối tượng với thuộc tính 'success' để chỉ ra đăng nhập thành công
-            if (data.success && username == username || password == password) {
+            // Kiểm tra xem đăng nhập thành công và có thông tin người dùng
+            if (data.success && data.user.token && data.user.role && username == username || password == password) {
                 alert('Đăng nhập thành công!');
-
-                // Redirect hoặc thực hiện hành động sau khi đăng nhập thành công
-                window.location.href = '../html/trang_home.html';
+                // Lưu token và vai trò vào localStorage
+                localStorage.setItem('Token', data.user.token);
+                localStorage.setItem('Role', data.user.role);
+                window.location.href ='../html/trang_home.html'
             } else {
                 alert('Tên đăng nhập hoặc mật khẩu không đúng, xin vui lòng nhập lại!');
             }
--- asset/JS/trang_home.js
+++ asset/JS/trang_home.js
@@ -1,20 +1,25 @@
-function phonglanhdao(){
+// Hàm xử lý khi người dùng nhấn vào nút Phòng lãnh đạo
+function phonglanhdao() {
     window.location.href = "../html/phonglanhdao.html";
 }
 
-function lanhdaodonvi(){
+// Hàm xử lý khi người dùng nhấn vào nút Lãnh đạo các đơn vị
+function lanhdaodonvi() {
     window.location.href = "../html/lanhdaodonvi.html";
 }
 
-function lanhdaotruong(){
+// Hàm xử lý khi người dùng nhấn vào nút Lãnh đạo trường
+function lanhdaotruong() {
     window.location.href = "../html/lanhdaotruong.html";
 }
 
-function cacnhansu(){
+// Hàm xử lý khi người dùng nhấn vào nút Các nhân sự
+function cacnhansu() {
     window.location.href = "../html/cacnhansu.html";
 }
 
-function helpme(){
+// Hàm xử lý khi người dùng nhấn vào nút Trợ giúp
+function helpme() {
     window.location.href = "../html/trogiup.html";
 }
 
@@ -25,7 +30,7 @@ function back() {
     window.location.href = "../html/lanhdaodonvi.html";
 }
 
-
+// bắt đầu chương trình đăng xuất
 // đăng xuất tài khoản
 
 function signout() {
@@ -48,3 +53,56 @@ history.pushState(null, null, location.href);
 window.onpopstate = function () {
     history.go(1);
 };
+
+
+// Gán sự kiện click cho nút Đăng xuất
+document.getElementById('signout').addEventListener('click', function() {
+    // Xóa token và vai trò từ localStorage
+    localStorage.removeItem('Token');
+    localStorage.removeItem('Role');
+    // Chuyển hướng người dùng đến trang đăng nhập
+    window.location.href = '../html/login.html';
+});
+
+const userRole = localStorage.getItem('Role');
+
+// Gọi hàm để hiển thị các nút tương ứng với vai trò của người dùng
+showButtonsForUserRole(userRole);
+
+// Hàm hiển thị các nút tương ứng với vai trò của người dùng
+function showButtonsForUserRole(userRole) {
+    switch (userRole) {
+        case 'admin':
+            break; // Không cần thay đổi gì vì admin có quyền truy cập vào tất cả các nút
+        case 'leader':
+            // Chặn sự kiện click của nút phonglanhdao
+            document.getElementById('phonglanhdao').addEventListener('click', function(event) {
+                event.preventDefault();
+                window.location.href = '../html/trang_home.html'
+                alert('Bạn không có quyền truy cập vào Phòng lãnh đạo!');
+            });
+            break;
+        case 'personnel':
+            // Chặn sự kiện click của nút phonglanhdao và lanhdaodonvi
+            document.getElementById('phonglanhdao').addEventListener('click', function(event) {
+                event.preventDefault();
+                window.location.href = '../html/trang_home.html'
+                alert('Bạn không có quyền truy cập vào Phòng lãnh đạo!');
+            });
+            document.getElementById('lanhdaodonvi').addEventListener('click', function(event) {
+                event.preventDefault();
+                alert('Bạn không có quyền truy cập vào Lãnh đạo các đơn vị!');
+            });
+            document.getElementById('lanhdaotruong').addEventListener('click', function(event) {
+                event.preventDefault();
+                alert('Bạn không có quyền truy cập vào Lãnh đạo trường!');
+            });
+            break;
+        default:
+            alert('Vai trò của bạn không được hỗ trợ!');
+            break;
+    }
+}
+
+
+
